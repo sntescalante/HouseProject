005f6bbf5a57694a90c727dff014ab077d7bc678
sntescalante <santiagoescalante2006@gmail.com>
2025-11-27 12:41:05 -0600

    Adapted save_to_db function for project's database

--- backend.py
+++ backend.py
@@ -18,7 +18,7 @@ MQTT_TOPIC = ["TEAM5/room1/actuator/OLED",
             "TEAM5/room3/actuator/Buzzer"] 
 
 
-# ----------- Config SQL (pymysql) -----------
+# ----------- Config SQL -----------
 connection = pymysql.connect(
     host="localhost",
     user="root",
@@ -39,45 +39,86 @@ def on_message(client, userdata, msg):
         topic = msg.topic
         value = msg.payload.decode().strip()
 
-        print(f"[MQTT] Topic: {topic} | Payload: {value}")
-
-        # -------------------------------
-        # LIMPIA EL MENSAJE RFID
-        # Ej: "RFID UID: 62:a:ce:6d" → "62:a:ce:6d"
-        # -------------------------------
-        if value.startswith("RFID UID:"):
-            uid = value.replace("RFID UID:", "").strip()
-        else:
-            uid = value
-
-        # Guardar solo el UID limpio
-        save_to_db(topic, uid)
+        save_to_db(topic, value)
 
     except Exception as e:
         print("Error procesando mensaje:", e)
 
-
-# ------------------ FUNCIÓN PARA GUARDAR EN MySQL ------------------
-def save_to_db(topic, uid):
+# ------------------ FUNCTION TO STORE DATA IN SQL DATABASE ------------------
+def save_to_db(topic, value):
     try:
+        parts = topic.split('/')  
+        location_str = parts[1]         # Room
+        device_kind = parts[2]          # Sensor or actuator
+        device_type = parts[3]          # Type
+
+        #To remove the room part from the topic to obtain the ID only
+        location_id = int(location_str.replace("room", "")) 
+
         with connection.cursor() as cursor:
-            sql = """
-                INSERT INTO rfid_logs (topic, uid)
-                VALUES (%s, %s)
-            """
-            cursor.execute(sql, (topic, uid))
-            print(f"[SQL] Guardado en BD → {topic}: {uid}")
+
+            #--------------------- If device is a SENSOR -----------------------------
+            if device_kind == "sensor":
+                cursor.execute("""
+                    SELECT SensorID FROM sensor
+                    WHERE LocationID = %s AND Type = %s
+                """, (location_id, device_type))
+                
+                result = cursor.fetchone()
+                if result:
+                    sensor_id = result["SensorID"]
+
+                    cursor.execute("""
+                        INSERT INTO sensorreadings (SensorID, Value)
+                        VALUES (%s, %s)
+                    """, (sensor_id, value))
+
+                    return
+                else:
+                    print(f"SENSOR NOT FOUND!!!.")
+                    return
+
+            # --------------------- ACTUATORS ---------------------
+            elif device_kind == "actuator":
+                cursor.execute("""
+                    SELECT ActuatorID FROM actuator
+                    WHERE LocationID = %s AND Type = %s
+                """, (location_id, device_type))
+                
+                result = cursor.fetchone()
+                if result:
+                    actuator_id = result["ActuatorID"]
+
+                    cursor.execute("""
+                        INSERT INTO actuatorvalues (ActuatorID, Value)
+                        VALUES (%s, %s)
+                    """, (actuator_id, value))
+
+                    print(f"[SQL] ACTUATOR → {device_type} (ID {actuator_id}) = {value}")
+                    return
+                else:
+                    print("[WARN] Actuador no encontrado en BD.")
+                    return
+
+            else:
+                print("[WARN] Topic desconocido →", topic)
 
     except Exception as e:
         print("Error al guardar en MySQL:", e)
 
-
 # ------------------ MQTT CLIENT ------------------
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
 
-client.connect(MQTT_BROKER, MQTT_PORT, keepalive=60)
+def main():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
 
-print("Escuchando mensajes MQTT...")
-client.loop_forever()
+    client.connect(MQTT_BROKER, MQTT_PORT, keepalive=60)
+
+    print("Escuchando mensajes MQTT...")
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    main()
+
